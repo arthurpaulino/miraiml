400f56ff6d0d20c25f4e93455a3ba22fb7d7a67f
Arthur Leonardo A. Paulino <arthur_paulino@virgo.atlantico.net.br>
2019-04-04 17:29:04 -0300

    fixing setup

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from distutils.core import setup
 setup(
     name = "MiraiML",
     version = "1.0",
-    packages = [
-        "miraiml",
-        "miraiml.core",
-        "miraiml.util",
-    ],
+    packages = ["miraiml"],
     author = "Arthur Paulino",
     author_email = "arthurleonardo.ap@gmail.com.com",
-    description = "An asynchronous engine for autonomous & continuous machine learning, built for real-time usage",
+    description = "An asynchronous engine for autonomous & continuous machine\
+        learning, built for real-time usage",
     url = "https://github.com/arthurpaulino/miraiml"
 )
