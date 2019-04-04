00a149dc3b8a556b831ac8574a4281d00f3747c5
Arthur Leonardo A. Paulino <arthur_paulino@virgo.atlantico.net.br>
2019-04-04 17:21:50 -0300

    typo

--- setup.py
+++ setup.py
@@ -11,6 +11,5 @@ setup(
     author = "Arthur Paulino",
     author_email = "arthurleonardo.ap@gmail.com.com",
     description = "An asynchronous engine for autonomous & continuous machine learning, built for real-time usage",
-    url = "https://github.com/arthurpaulino/miraiml",
-    },
+    url = "https://github.com/arthurpaulino/miraiml"
 )
